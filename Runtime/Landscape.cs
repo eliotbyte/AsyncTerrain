1bc3adf0811255d3b28a8a4c5c7467866a418108
Eliot Byte <eliotbyte@proton.me>
2024-07-09 21:25:09 +0300

    Moved mesh generation from Landscape class to MeshPlane class

--- Runtime/Landscape.cs
+++ Runtime/Landscape.cs
@@ -4,17 +4,14 @@ namespace EliotByte.AsyncTerrain
 {
     public class Landscape : MonoBehaviour
     {
-        private MeshFilter meshFilter;
-        private MeshRenderer meshRenderer;
+        private MeshFilter _meshFilter;
+        private MeshRenderer _meshRenderer;
 
         public bool Initialized { get; private set; } = false;
-        public int Width { get; private set; }
-        public int Height { get; private set; }
         public float[,] Heights { get; private set; }
         public Material Material { get; private set; }
         public Vector3 Size { get; private set; }
         public Mesh Mesh { get; private set; }
-        public Vector3[] Vertices { get; private set; }
 
         public void Initialize(Material material)
         {
@@ -23,10 +20,10 @@ namespace EliotByte.AsyncTerrain
 
             Material = material;
 
-            meshFilter = gameObject.AddComponent<MeshFilter>();
-            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            _meshFilter = gameObject.AddComponent<MeshFilter>();
+            _meshRenderer = gameObject.AddComponent<MeshRenderer>();
 
-            meshRenderer.material = Material;
+            _meshRenderer.material = Material;
 
             Initialized = true;
         }
@@ -42,91 +39,25 @@ namespace EliotByte.AsyncTerrain
 
             Heights = heights;
             Size = size;
-            int newWidth = Heights.GetLength(0);
-            int newHeight = Heights.GetLength(1);
 
-            if (newWidth != Width || newHeight != Height || Mesh == null)
-            {
-                Width = newWidth;
-                Height = newHeight;
+            if (Mesh != null )
                 Destroy(Mesh);
-                Mesh = null;
-                GenerateMesh();
-            }
-            else
-            {
-                UpdateMesh();
-            }
-        }
 
-        private void GenerateMesh()
-        {
-            Mesh = new Mesh();
-            Vertices = new Vector3[Width * Height];
-            Vector2[] uv = new Vector2[Width * Height];
-            int[] triangles = new int[(Width - 1) * (Height - 1) * 6];
+            MeshPlane plane = new MeshPlane(heights, size);
 
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    int index = x + y * Width;
-                    Vertices[index] = new Vector3(x * Size.x / (Width - 1), Heights[x, y] * Size.y,
-                        y * Size.z / (Height - 1));
-                    uv[index] = new Vector2((float)x / (Width - 1), (float)y / (Height - 1));
-                }
-            }
+            plane.Generate();
 
-            int triIndex = 0;
-            for (int x = 0; x < Width - 1; x++)
-            {
-                for (int y = 0; y < Height - 1; y++)
-                {
-                    int current = x + y * Width;
-                    int next = current + Width;
-
-                    triangles[triIndex] = current;
-                    triangles[triIndex + 1] = next;
-                    triangles[triIndex + 2] = current + 1;
-
-                    triangles[triIndex + 3] = current + 1;
-                    triangles[triIndex + 4] = next;
-                    triangles[triIndex + 5] = next + 1;
-
-                    triIndex += 6;
-                }
-            }
-
-            Mesh.vertices = Vertices;
-            Mesh.uv = uv;
-            Mesh.triangles = triangles;
-            Mesh.RecalculateNormals();
-
-            meshFilter.mesh = Mesh;
-        }
-
-        private void UpdateMesh()
-        {
-            for (int x = 0; x < Width; x++)
-            {
-                for (int y = 0; y < Height; y++)
-                {
-                    Vertices[x + y * Width].y = Heights[x, y] * Size.y;
-                }
-            }
-
-            Mesh.vertices = Vertices;
-            Mesh.RecalculateNormals();
-            Mesh.RecalculateBounds();
+            Mesh = plane.GenerateMesh();
+            _meshFilter.mesh = Mesh;
         }
 
         private void OnDestroy()
         {
-            if (Mesh != null)
-            {
-                Destroy(Mesh);
-                Mesh = null;
-            }
+            if (Mesh == null)
+                return;
+
+            Destroy(Mesh);
+            Mesh = null;
         }
     }
 }
---
+++ Runtime/MeshPlane.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+namespace EliotByte.AsyncTerrain
+{
+    public class MeshPlane
+    {
+        private float[,] _heights;
+        private readonly Vector3 _meshSize;
+        private readonly Vector2Int _gridSize;
+        private readonly Vector3[] _vertices;
+        private readonly int[] _triangles;
+        private readonly Vector2[] _uvs;
+
+        public MeshPlane(float[,] heights, Vector3 meshSize)
+        {
+            _heights = heights;
+            _meshSize = meshSize;
+            _gridSize = new Vector2Int(heights.GetLength(0), heights.GetLength(1));
+            _vertices = new Vector3[_gridSize.x * _gridSize.y];
+            _triangles = new int[(_gridSize.x - 1) * (_gridSize.y - 1) * 6];
+            _uvs = new Vector2[_gridSize.x * _gridSize.y];
+        }
+
+        public void Generate()
+        {
+            for (int x = 0; x < _gridSize.x; x++)
+            {
+                for (int y = 0; y < _gridSize.y; y++)
+                {
+                    int index = x + y * _gridSize.x;
+                    _vertices[index] = new Vector3(x * _meshSize.x / (_gridSize.x - 1), _heights[x, y] * _meshSize.y,
+                        y * _meshSize.z / (_gridSize.y - 1));
+                    _uvs[index] = new Vector2((float)x / (_gridSize.x - 1), (float)y / (_gridSize.y - 1));
+                }
+            }
+
+            int triIndex = 0;
+            for (int x = 0; x < _gridSize.x - 1; x++)
+            {
+                for (int y = 0; y < _gridSize.y - 1; y++)
+                {
+                    int current = x + y * _gridSize.x;
+                    int next = current + _gridSize.x;
+
+                    _triangles[triIndex] = current;
+                    _triangles[triIndex + 1] = next;
+                    _triangles[triIndex + 2] = current + 1;
+
+                    _triangles[triIndex + 3] = current + 1;
+                    _triangles[triIndex + 4] = next;
+                    _triangles[triIndex + 5] = next + 1;
+
+                    triIndex += 6;
+                }
+            }
+        }
+
+        public Mesh GenerateMesh()
+        {
+            Mesh mesh = new() {vertices = _vertices, uv = _uvs, triangles = _triangles};
+            mesh.RecalculateNormals();
+
+            return mesh;
+        }
+
+        private void Update(float[,] heights)
+        {
+            _heights = heights;
+
+            for (int x = 0; x < _gridSize.x; x++)
+            for (int y = 0; y < _gridSize.y; y++)
+                _vertices[x + y * _gridSize.x].y = _heights[x, y] * _meshSize.y;
+        }
+    }
+}
